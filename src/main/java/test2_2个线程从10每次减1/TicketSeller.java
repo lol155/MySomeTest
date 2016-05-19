f9156b9f414446f5e9f64c5b534b682a4aa0c1cd
lol155 <1055455968@qq.com>
2016-05-19 22:18:51 +0800

    售票-线程交替售票

--- src/main/java/test2_2个线程从10每次减1/TicketSeller.java
+++ src/main/java/test2_2个线程从10每次减1/TicketSeller.java
@@ -1,10 +1,4 @@
 package test2_2个线程从10每次减1;
-
-import java.nio.channels.Selector;
-
-/**
- * Created by admin on 2016/5/19.
- */
 public class TicketSeller implements Runnable{
     private int count = 0 ;
     public TicketSeller(int i) {
@@ -13,17 +7,22 @@ public class TicketSeller implements Runnable{
     }
 
     public void run() {
-
-        while(this.count > 0 ){
-            synchronized (this){
-                if(this.count > 0){
-                    System.out.println(Thread.currentThread().getId() + "售出一张票！剩余" + --this.count +"张票！");
-                }else{
-                    System.out.println("票已售完，对不起！");
+        try {
+            while(this.count > 0 ){
+                synchronized (this){
+                    notifyAll();
+                    if(this.count > 0){
+                        System.out.println(Thread.currentThread().getId() + "售出一张票！剩余" + --this.count +"张票！");
+                        wait();
+                    }else{
+                        System.out.println("票已售完，对不起！");
+                    }
                 }
             }
-        }
-        System.out.println("票已全部售完！");
+            System.out.println("票已全部售完！");
 //        notifyAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }

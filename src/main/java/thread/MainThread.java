1bc60ab894d0c3cefedd07a28b025fe1b5d74de1
lol155 <1055455968@qq.com>
2016-09-16 16:42:06 +0800

    thread

---
+++ src/main/java/thread/MainThread.java
@@ -0,0 +1,11 @@
+package thread;
+
+/**
+ * Created by admin on 2016/9/16.
+ */
+public class MainThread {
+    public static void main(String[] args) {
+        LiftOff launch = new LiftOff();
+        launch.run();
+    }
+}
